b7ea3cfcf2857a226cb580b44e291c6adc535b2c
A.Kerem DARICIK <A.Kerem DARICIK@DESKTOP-DHVMOCV>
2021-12-08 18:04:33 +0300

    Add project files.

---
+++ ConsoleApp14/Müşteriler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Müşteriler
+    {
+        public int HesapNo { get; set; }
+        public string İsim { get; set; }
+        public string Soyisim { get; set; }
+        public string Malvarlıkları  { get; set; }
+    }
+}
---
+++ ConsoleApp14/İşlemNoktası.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class İşlemNoktası
+    {
+        static void Main(string[] args) 
+        {
+            Müşteriler müşteriler = new Müşteriler();
+            müşteriler.HesapNo = 292636;
+            müşteriler.İsim = "Fatih";
+            müşteriler.Soyisim = "Gart";
+            müşteriler.Malvarlıkları = "Nike Air Jordan(Sınırsız)";
+            Console.WriteLine(müşteriler.HesapNo);
+            Console.WriteLine(müşteriler.İsim);
+            Console.WriteLine(müşteriler.Soyisim);
+            Console.WriteLine(müşteriler.Malvarlıkları);
+            Console.ReadLine();
+
+        }        
+    }
+
+       
+}
